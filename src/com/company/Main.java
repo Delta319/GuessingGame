cb3f91535999e95fb1af08715241096292fb85c4
Ethan Rowe <redsoxrowe@gmail.com>
2018-06-12 13:51:49 -0400

    Added game procedures and hints

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+    /*How to get a random number
+
 
         double randomNumber;
         randomNumber = Math.random();
@@ -18,7 +22,30 @@ public class Main {
         System.out.println("The Rounded to Floor is: " + randomNumberRoundedToFloor);
 
         int randomNumberRoundedNormally = (int) Math.round(randomNumberTimesHundred);
-        System.out.println("The Rounding that we're used to: " + randomNumberRoundedNormally);
+        System.out.println("The Rounding that we're used to: " + randomNumberRoundedNormally);*/
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Guessing game! I'm thinking of a number between 1 and 100. Can you figure it out?");
+
+        int randomNumber = (int) Math.ceil(Math.random() * 100);
+        System.out.println(randomNumber);
+
+        int guessedNumber;
+        System.out.println("Guess a number between 1 and 100");
+        guessedNumber = input.nextInt();
+        System.out.println("You guessed the number " + guessedNumber);
+
+        if (randomNumber > guessedNumber) {
+            System.out.println("Your guess is too low. Guess higher.");
+
+        } else if (randomNumber < guessedNumber) {
+            System.out.println("Your guess is too high. Guess lower.");
+
+        } else {
+            System.out.println("Congratulations! You guessed my number " + randomNumber + "!");
+        }
+
     }
 }
 
